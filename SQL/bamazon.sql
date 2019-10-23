2bfe74281fcccbdf98832d8d713658f65a9a0cc7
Michael-Beebe <50559626+Michael-Beebe@users.noreply.github.com>
2019-10-23 10:11:43 -0400

    Update bamazon.sql

---
+++ SQL/bamazon.sql
@@ -0,0 +1,44 @@
+-- ####### Create bamazon DB #####
+-- CREATE DATABASE bamazon;
+
+####### Create a products table within the bamazon DB.########
+-- USE bamazon;
+-- CREATE TABLE products (
+-- 	item_id INTEGER auto_increment NOT NULL,
+--     product_name TEXT,
+--     department_name TEXT,
+-- 	price DECIMAL (13,2),
+--     stock_quantity INTEGER,
+--     PRIMARY KEY (item_id)
+-- );
+
+-- ######### Inserting 10 items into the products table ########
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Monopoly", "Board Games", 9.99, 25);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Risk", "Board Games", 9.99, 25);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Overwatch", "Video Games", 59.99, 30);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("DOOM", "Video Games", 59.99, 28);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Cakes", "Food and Drink", 9.99, 10);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Gatorade", "Food and Drink", 1.99, 300);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("55' Samsung TV", "Electronics", 599.99, 15);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Playstation 4", "Video Games", 399.99, 10);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("XBOX One", "Video Games", 449.99, 10);
+
+-- INSERT INTO bamazon.products (product_name, department_name, price, stock_quantity)
+-- VALUES ("Nintendo Switch", "Video Games", 299.99, 15);
