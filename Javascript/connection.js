a42e7af770221e7acecf41159e1922d6397f33b2
Michael Beebe <michaelbeebe@michaels-mbp.home>
2019-10-24 21:15:25 -0400

    update

--- Javascript/connection.js
+++ Javascript/connection.js
@@ -36,3 +36,15 @@ var queryAllProducts = () => {
         console.log("\n============================================\n");
     })
 }
+
+// inquirer code
+inquirer.prompt([
+    {
+        type: "list",
+        name: "managerChoices",
+        message: "What would you like to do?",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+    }
+]).then(function(){
+
+});
