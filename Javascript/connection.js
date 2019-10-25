970c5acbff9a8c09b921c2996313961bd3d13f63
Michael Beebe <michaelbeebe@Michaels-MBP.lan>
2019-10-25 11:34:35 -0400

    2/4 functions are wokring properly

--- Javascript/connection.js
+++ Javascript/connection.js
@@ -2,6 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 // ESTABLISHING CONNECTION TO DATABASE
+// ---------------------------------------------------------------------------------------------------
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -17,18 +18,50 @@ connection.connect(function(err, response) {
     else {
         console.log("\nconnected as id " + connection.threadId);
         console.log("============================================\n")
-        queryAllProducts();
+
+        // INQUIRER CODE
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "managerChoices",
+                message: "What would you like to do?",
+                choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+            }
+        ])
+        // ****** IF STATEMENTS THAT RUN THE FUNCTION THAT THE USER CHOOSES ********
+        // 
+        .then(function(){
+            if ("View Products for Sale") {
+                viewProductsForSale();
+            }
+            if ("View Low Inventory") {
+                viewLowInventory();
+            }
+            if ("Add to Inventory") {
+                addToInventory();
+            }
+            if ("Add New Product") {
+                addNewProduct();
+            }
+        });
     }
   });
+
+
 // ========================================================================
 
-// queries stored in vars
-var selectAllProducts = "SELECT * FROM bamazon.products"
-
-// 
-var queryAllProducts = () => {
+// QUERIES
+// -------------------------------------------------------------------------------------------------
+var selectAllProducts = "SELECT * FROM bamazon.products";
+var selectLowInventory = "SELECT * FROM bamazon.products WHERE stock_quantity < 5";
+// TODO: store user inputs into this variable to add values to the SQL query
+var insertInto = "INSERT INTO bamazon.products" ;
+// FUNCTIONS
+// -------------------------------------------------------------------------------------------------
+// DISPLAYS ALL PRODUCTS
+var viewProductsForSale = () => {
     connection.query(selectAllProducts, function(err, response) {
-        console.log("id | Product | Department | Price | Stock" )
+        console.log("\nid | Product | Department | Price | Stock" )
         console.log("-------------------------------------------")
         for (var i = 0; i < response.length; i++) {
             console.log(response[i].item_id + " | " + response[i].product_name + " | " + response[i].department_name + " | " + response[i].price + " | " + response[i].stock_quantity)
@@ -37,14 +70,24 @@ var queryAllProducts = () => {
     })
 }
 
-// inquirer code
-inquirer.prompt([
-    {
-        type: "list",
-        name: "managerChoices",
-        message: "What would you like to do?",
-        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
-    }
-]).then(function(){
-    
-});
+// TODO: DISPLAYS PRODUCTS WITH STOCK LESS THAN 5
+var viewLowInventory = () => {
+    connection.query(selectLowInventory, function(err, response) {
+        console.log("\nid | Product | Department | Price | Stock" );
+        console.log("-------------------------------------------")
+        for (var i = 0; i < response.length; i++) {
+            console.log(response[i].item_id + " | " + response[i].product_name + " | " + response[i].department_name + " | " + response[i].price + " | " + response[i].stock_quantity)
+        }
+        console.log("\n============================================\n");
+    })
+}
+
+// TODO: GIVES OPTION TO ADD STOCK TO A PRODUCT
+var addToInventory = () => {
+
+}
+
+// TODO: GIVES OPTION TO ADD A NEW PRODUCT
+var addNewProduct = () => {
+
+}
