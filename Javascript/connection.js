0b983a5033edf19b3c80401e860e36388078bf41
Michael Beebe <michaelbeebe@Michaels-MacBook-Pro.local>
2019-10-25 22:10:28 -0400

    10/25 10:10pm

--- Javascript/connection.js
+++ Javascript/connection.js
@@ -30,25 +30,27 @@ connection.connect(function(err, response) {
             },
         ])
         // ****** IF STATEMENTS THAT RUN THE FUNCTION THAT THE USER CHOOSES ********
-        // FIXME: all functions are running when user picks an option, need to add some validation to only run the function that the user selects
+        // FIXME: make it so when node runs one command it allows the user to run another command
         .then(function(user){
             if (user.managerChoices === "View Products for Sale") {
-                viewProductsForSale();
+                displayTable();
             }
             if (user.managerChoices === "View Low Inventory") {
-                viewLowInventory();
+                displayLowInventory();
             }
             if (user.managerChoices === "Add to Inventory") {
+                displayProductList();
                 inquirer.prompt([
                     {
                         type: "list",
                         name: "product list",
-                        message: "Which product would you like to add?"
+                        // FIXME: THIS NEEDS TO GIVE A LIST OF THE PRODUCTS
+                        choices: [productList],
+                        message: "Which product would you like to add inventory to?"
                     }
                 ])
                 addInventory();
                 displayNewInventory();
-
             }
             if (user.managerChoices === "Add New Product") {
                 inquirer.prompt([
@@ -81,32 +83,31 @@ connection.connect(function(err, response) {
     }
   });
 
-
 // ========================================================================
-
 // QUERIES
 // -------------------------------------------------------------------------------------------------
-var selectAllProducts = "SELECT * FROM bamazon.products";
-var selectLowInventory = "SELECT * FROM bamazon.products WHERE stock_quantity < 5";
+
 // TODO: store user inputs into this variable to add values to the SQL query
 var insertInto = "INSERT INTO bamazon.products" ;
 
 // FUNCTIONS
 // -------------------------------------------------------------------------------------------------
 // DISPLAYS ALL PRODUCTS
-var viewProductsForSale = () => {
+var selectAllProducts = "SELECT * FROM bamazon.products";
+var array = [];
+var displayTable = () => {
     connection.query(selectAllProducts, function(err, response) {
         console.log("\nid | Product | Department | Price | Stock" )
         console.log("-------------------------------------------")
         for (var i = 0; i < response.length; i++) {
-            console.log(response[i].item_id + " | " + response[i].product_name + " | " + response[i].department_name + " | " + response[i].price + " | " + response[i].stock_quantity)
-        }
-        console.log("\n============================================\n");
+            console.log(response[i].item_id + " | " + response[i].product_name + " | " + response[i].department_name + " | " + response[i].price + " | " + response[i].stock_quantity)        }
+            console.log("\n============================================\n");
     })
 }
 
-// TODO: DISPLAYS PRODUCTS WITH STOCK LESS THAN 5
-var viewLowInventory = () => {
+// DISPLAYS PRODUCTS WITH STOCK LESS THAN 5
+var selectLowInventory = "SELECT * FROM bamazon.products WHERE stock_quantity < 5";
+var displayLowInventory = () => {
     connection.query(selectLowInventory, function(err, response) {
         console.log("\nid | Product | Department | Price | Stock" );
         console.log("-------------------------------------------")
@@ -117,17 +118,33 @@ var viewLowInventory = () => {
     })
 }
 
+// TODO: DISPLAY PRODUCT LIST
+var selectProductNames = "SELECT product_name FROM bamazon.products";
+var productList = [];
+var displayProductList = () => {
+    connection.query(selectProductNames, function(err, response) {
+        for (var i = 0; i < response.length; i++) {
+            // console.log(response[i]);
+            productList[i] = response[i].product_name;
+        }
+        for (var j = 0; j < productList.length; j++) {
+            // console.log(productList[j]);
+        }
+        console.log("\n============================================\n");
+    })
+}
+
 // TODO: GIVES OPTION TO ADD STOCK TO A PRODUCT
 var addInventory = () => {
+    
+}
+
+// TODO: DISPLAYS NEW INVENTORY AFTER USER ADDS A PRODUCT TO THE TABLE
+var displayNewInventory = () => {
 
 }
 
 // TODO: GIVES OPTION TO ADD A NEW PRODUCT
 var addNewProduct = () => {
-
-}
-
-// TODO: DISPLAYS NEW INVENTORY AFTER USER ADDS A PRODUCT TO THE TABLE
-var displayNewInventory = () => {
 
 }
